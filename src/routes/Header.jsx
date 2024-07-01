f9acd2c92279e41779684c042d668db8a7743480
Brick Sigma <bricksigma176@gmail.com>
2024-07-01 11:23:22 +0300

    Added CV

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -7,6 +7,10 @@ import ItchIOWhite from '../assets/links/itchio-logo-textless-white.png';
 import ItchIODark from '../assets/links/itchio-logo-textless-black.png';
 import LinkedInWhite from '../assets/links/In-White-128.png';
 import LinkedInBlue from '../assets/links/In-Blue-128.png';
+import CVWhite from '../assets/links/cv-white.png';
+import CVDark from '../assets/links/cv-dark.png';
+
+import CV from '../assets/cv.pdf';
 
 import { HashLink } from "react-router-hash-link";
 
@@ -49,6 +53,7 @@ const Header = () => {
                 <Icon href="https://github.com/BrickSigma" alt="GitHub" dark={GitHubDark} white={GitHubWhite} scrolled={scrollStyle} />
                 <Icon href="https://bricksigma.itch.io/" alt="Itch.io" dark={ItchIODark} white={ItchIOWhite} scrolled={scrollStyle} />
                 <Icon href="https://www.linkedin.com/in/mohamed-junaid-chaudhry-458b4b208/" alt="LinkedIn" dark={LinkedInBlue} white={LinkedInWhite} scrolled={scrollStyle} />
+                <Icon href={CV} alt="CV" dark={CVWhite} white={CVDark} scrolled={scrollStyle} />
             </div>
         </div>
     )
